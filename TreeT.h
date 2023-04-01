246c3e40e99896e3a81e306c7a8a32b413840f7e
Tri <trimpham98@gmail.com>
2023-04-01 10:57:01 -0700

    Add public function pre/post conditions

--- TreeT.h
+++ TreeT.h
@@ -18,8 +18,8 @@ class TreeT {
 public:
     TreeT();
     // Preconditions: None.
-    // Postconditions: A new binary search tree object is created with root pointer set
-    // to null and numNodes set to 0.
+    // Postconditions: A new binary search tree object is created
+    // with root pointer set to null and numNodes set to 0.
 
     ~TreeT();
     // Preconditions: None.
@@ -28,16 +28,48 @@ public:
     // set to 0.
 
     TreeT& operator=(const TreeT& otherTree);
+    // Precondition: A valid TreeT object exists,
+    // and another valid TreeT object is provided.
+    // Postcondition: The current tree is destroyed and replaced with
+    // a deep copy of the provided tree.
 
     void Add(T value);       // Add value to the tree
+    // Precondition: A valid TreeT object exists,
+    // and a value of type T is provided.
+    // Postcondition: A new node with the provided value is added
+    // to the tree while maintaining the BST property.
+    // The number of nodes in the tree is incremented by 1.
+
     void Remove(T value);    // Remove value from the tree
+    // Precondition: A valid TreeT object exists,
+    // and a value of type T is provided.
+    // Postcondition: If a node with the provided value is found in the tree,
+    // the node is removed while maintaining the BST property, and the number
+    // of nodes is decremented by 1. If the value is not found, the tree
+    // remains unchanged.
+
     bool Contains(T value);  // Determines if value is in the tree
+    // Precondition: A valid TreeT object exists, and a value of type T is provided.
+    // Postcondition: Returns true if the provided value is found in the tree,
+    // and false otherwise. The tree remains unchanged.
 
     int Size();     // Number of values in the tree
+    // Precondition: A valid TreeT object exists.
+    // Postcondition: Returns the number of nodes in the tree.
+    // The tree remains unchanged.
 
     // These are used by the iterator
     void ResetIterator(Order traverseOrder);    // Initializes the Iterator
+    // Precondition: A valid TreeT object exists, and a valid traverse
+    // order is provided.
+    // Postcondition: The iterator queue is cleared and filled with node values
+    // based on the given traverse order. The tree remains unchanged.
+
     T GetNextItem();
+    // Precondition: A valid TreeT object exists, and ResetIterator has
+    // been called with a valid traverse order.
+    // Postcondition: Returns the next item from the iterator queue and
+    // removes it from the queue. The tree remains unchanged.
 
 private:
     struct Node {
